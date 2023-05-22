13d2ebf881205185062d5d5e9259b8167e314bdb
aldeisebarbosa@gmail.com <aldeisebarbosa@gmail.com>
2023-05-21 22:32:58 -0300

    criando o select Country

---
+++ meu-time/src/components/CountrySelector.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import api from '../api';
+
+interface Country {
+  country_id: number;
+  country_name: string;
+}
+
+const CountrySelector: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await api.get('/countries');
+        setCountries(response.data.response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  const handleCountrySelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCountryId = parseInt(event.target.value);
+    // Lógica para selecionar o país
+    // Você pode usar o ID do país selecionado para fazer requisições adicionais à API-Football
+  };
+
+  return (
+    <div>
+      <h2>Select Country</h2>
+      <select onChange={handleCountrySelect}>
+        <option value="">Select a country</option>
+        {countries.map((country) => (
+          <option key={country.country_id} value={country.country_id}>
+            {country.country_name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default CountrySelector;
